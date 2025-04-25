c028ac977330f5fdc83f1b260a6cf0a3b41bb1cd
ton-long <114124041+ton-long@users.noreply.github.com>
2025-04-25 00:19:05 -0300

    BugFix

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./reset.module.scss";
+import "./reset.scss";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
