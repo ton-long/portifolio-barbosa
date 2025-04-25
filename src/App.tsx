eecfa3b736a2b3dbf4fc034b200fd3220e5cd152
ton-long <114124041+ton-long@users.noreply.github.com>
2025-04-25 00:10:28 -0300

    Bugfix: corrigindo sytles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { RecoilRoot } from "recoil";
-import "./App.module.scss";
+import styles from "./App.module.scss";
 import TaskBar from "./components/TaskBar";
 import Desktop from "./components/Desktop";
 
 const App = () => {
   return (
     <RecoilRoot>
-      <Desktop />
-      <TaskBar />      
+      <div className={styles.body}>
+        <Desktop />
+        <TaskBar />
+      </div>
     </RecoilRoot>
   );
 };
