a2f34508ddf502c9d9c015da5a5873f3be4d5d09
ton-long <114124041+ton-long@users.noreply.github.com>
2025-04-26 12:07:16 -0300

    refactor: melhora redação do texto

--- src/components/IconsComponents/AboutMe/index.tsx
+++ src/components/IconsComponents/AboutMe/index.tsx
@@ -3,9 +3,9 @@ import styles from "./AboutMe.module.scss";
 const AboutMe = () => {
   return (
     <div className={styles.mainAboutMe}>
-      <hr className={styles.hrTitle}/>
+      <hr className={styles.hrTitle} />
       <h1 className={styles.aboutMeTitle}>Olá, me chamo Luã!</h1>
-      <hr className={styles.hrTitle}/>
+      <hr className={styles.hrTitle} />
       <p className={styles.aboutMeP}>
         Sempre tive uma paixão por tecnologia e programação, mas foi durante a
         pandemia que decidi seguir esse caminho de forma mais séria. A ideia de
@@ -13,22 +13,21 @@ const AboutMe = () => {
         por conta própria, dedicando meu tempo a aprender e aprimorar minhas
         habilidades.
       </p>
-      <hr className={styles.hr}/>
+      <hr className={styles.hr} />
       <p className={styles.aboutMeP}>
         Inicialmente, foi um desafio, mas com o tempo, a programação passou a me
         fascinar cada vez mais. A sensação de resolver problemas e criar
-        soluções me trouxe mais próximo da área. Com o tempo, resolvi me aprofundar ainda mais e investi em
-        diversos cursos, que me permitiram melhorar meus conhecimentos e me
-        tornar um desenvolvedor melhor.
+        soluções me trouxe mais próximo da área. Com o tempo, resolvi me
+        aprofundar ainda mais e investi em diversos cursos, que me permitiram
+        melhorar meus conhecimentos e me tornar um desenvolvedor melhor.
       </p>
-      <hr className={styles.hr}/>
+      <hr className={styles.hr} />
       <p className={styles.aboutMeP}>
-        Hoje, a programação é algo que me apaixona a cada novo projeto, e estou
+        Hoje, a programação é algo que me encanta a cada novo projeto, e estou
         sempre em busca de novos aprendizados e desafios. Estou pronto para
         aplicar tudo o que aprendi em projetos reais e continuar crescendo na
         carreira de desenvolvedor.
       </p>
-      
     </div>
   );
 };
--- src/components/IconsComponents/System/index.tsx
+++ src/components/IconsComponents/System/index.tsx
@@ -4,20 +4,27 @@ const System = () => {
   return (
     <div className={styles.mainSystem}>
       <h1>C:\&gt; SOBRE O SISTEMA.</h1>
-      <p>&gt; Site portifólio com design retrô baseado em um sistema operacional de 1995.</p>
+      <p>
+        &gt; Site portifólio com design retrô baseado em um sistema operacional
+        de 1995.
+      </p>
       <p>
         &gt; Para melhor experiência do usuário, é aconselhável que esse site
         seja aberto em um computador.
       </p>
       <p>
-        &gt; As janelas e a barra de menu simulam funcionalidades de um sistema operacional clássico.
+        &gt; As janelas e a barra de menu simulam funcionalidades de um sistema
+        operacional clássico.
       </p>
       <p>&gt; Botão Iniciar e todos os ícones funcionais.</p>
       <p>
         &gt; Possui aplicações como Campo Minado e Calculadora, com
         comportamentos semelhantes às da época.
       </p>
-      <p>&gt; Tecnologias usadas: Vite, React, TypeScript, Recoil, react-rnd.</p>
+      <p>&gt; Você encontrará meu LinkedIn e GitHub no menu Iniciar.</p>
+      <p>
+        &gt; Tecnologias usadas: Vite, React, TypeScript, Recoil, react-rnd.
+      </p>
       <p className={styles.pSystem}>Espero que gostem!!</p>
     </div>
   );
